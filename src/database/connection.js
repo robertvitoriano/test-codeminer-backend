fc240351e0a36bd6bd317d06291fa7f5841ce969
robertvitoriano <robertvitorianodev@gmail.com>
2020-08-19 17:36:59 -0300

    trying to connect to mongoDB

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,4 +1,9 @@
 const mongoose = require('mongoose');
 
 
-
+mongoose.connect('mongodb+srv://robertvitoriano:961862194@cluster0-btwq6.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+},()=>{console.log('Im connected to mongodb')});
--- src/models/survior.js
+++ src/models/survior.js
@@ -5,11 +5,21 @@ const Schema = mongoose.Schema;
 const userSchema = new mongoose.Schema({
     name:{
 
+        type:String,
+        required:true,
     },
     age:{
-
+        type:Number,
+        required:true
     },
     gender:{
-        
+        type:String,
+        required:true
+
+    },
+    lastLocation:{
+        type: String,
+        required:true
+
     }
 })
