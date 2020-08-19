cb452d50826ca56d69ee348b250f8cb470f5d280
robertvitoriano <robertvitorianodev@gmail.com>
2020-08-19 17:24:11 -0300

    creating survivor model

--- src/models/survior.js
+++ src/models/survior.js
@@ -1,2 +1,15 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
+
+const userSchema = new mongoose.Schema({
+    name:{
+
+    },
+    age:{
+
+    },
+    gender:{
+        
+    }
+})
