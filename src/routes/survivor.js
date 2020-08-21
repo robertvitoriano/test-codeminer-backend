b08d452eb12ebf2408be1d2254b42d87b165b592
robertvitoriano <robertvitorianodev@gmail.com>
2020-08-21 09:51:33 -0300

    organizing code

--- src/routes/survivor.js
+++ src/routes/survivor.js
@@ -2,8 +2,8 @@ const survivorRouter = require('express').Router();
 const survivorController = require('./../controllers/survivor')
 
 
-survivorRouter.get('/:id',survivorController.getSurvivorById);
-survivorRouter.post('/survivors',survivorController.registerSurvivor);
+survivorRouter.get('/:id', survivorController.getSurvivorById);
+survivorRouter.post('/survivors', survivorController.registerSurvivor);
 survivorRouter.get('/survivors/all', survivorController.getAllSurvivors);
 survivorRouter.post('/survivor/:survivorId', survivorController.tradeItems);
 
